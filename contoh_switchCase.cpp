e4b2a67103aa9930e63fad59f777b3b79cc483b6
MuhAminSyam <aminsyammuh@gmail.com>
2025-03-17 12:07:12 +0700

    switch case

--- contoh_switchCase.cpp
+++ contoh_switchCase.cpp
@@ -15,4 +15,38 @@ int main()
     int pilihan;
     float panjang, lebar;
     float jejari;
+
+    do
+    {
+        cout << "menu" << endl;
+        cout << "1. Luas Persegi Panjang" << endl;
+        cout << "2. Luas Lingkaran" << endl;
+        cout << "3. Kelaur" << endl;
+
+        cout << "Pilihan (1/2/3) = ";
+        cin >> pilihan;
+
+        switch (pilihan)
+        {
+        case 1:
+            cout << "Hitung luas persegi panjang" << endl;
+            cout << "Masukkan panjang = ";
+            cin >> panjang;
+            cout << "Masukkan lebar = ";
+            cin >> lebar;
+            cout << "Luas persegi panjang = " << LuasPersegiPanjang(panjang, lebar) << endl;
+            break;
+        case 2:
+            cout << "Hitung luas lingkaran" << endl;
+            cout << "Masukkan jari-jari = ";
+            cin >> jejari;
+            cout << "Luas lingkaran= " << LuasLingkaran(jejari) << endl;
+            break;
+        case 3:
+            break;
+        default:
+            cout << "Pilihan salah, pilihan harus di antara 1-3.";
+            break;
+        }
+    } while (pilihan != 3);
 }
