d45e88646c6e27616058e405b11c38eda036d5bf
MuhAminSyam <aminsyammuh@gmail.com>
2025-03-17 11:08:46 +0700

    perulangan for pencacah turun

--- contoh_Looping.cpp
+++ contoh_Looping.cpp
@@ -18,4 +18,13 @@ int main()
     }
     cout << "Nilai n terakhir =" << n << endl;
     cout << endl;
+
+    cout << "perulangan for pencacah turun" << endl;
+    // perulangan dengan for
+    for (n = 5; n > 0; n--)
+    {
+        cout << "Nilai n = " << n << "Selamat Datang" << endl;
+    }
+    cout << "Nilai n terakhir = " << n;
+    cout << endl;
 }
